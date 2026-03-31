761a1dabb9ec72443d62be0b82d99402e001aa73
ymrna <ymrnaa@gmail.com>
2026-03-31 08:46:30 +0700

    prepare deploy

--- app/main.py
+++ app/main.py
@@ -5,8 +5,19 @@ from fastapi.templating import Jinja2Templates
 
 from app.models import Points
 from app.optimizer import cluster_points
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(title="SFR Coverage Optimizer")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # bisa dibatasi nanti
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/coverage/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
