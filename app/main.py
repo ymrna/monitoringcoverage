b70f0c3abe43980f249bca3c63fb79d76feb99a0
ymrna <ymrnaa@gmail.com>
2026-04-13 11:48:45 +0700

    update path

--- app/main.py
+++ app/main.py
@@ -2,25 +2,34 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+
+import os  # ✅ WAJIB
 
 from app.models import Points
 from app.optimizer import cluster_points
-from fastapi.middleware.cors import CORSMiddleware
-
-
 
 app = FastAPI(title="SFR Coverage Optimizer")
 
+# ✅ FIX PATH ABSOLUTE
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# ✅ STATIC FIX
+app.mount(
+    "/static",
+    StaticFiles(directory=os.path.join(BASE_DIR, "static")),
+    name="static"
+)
 
-app.mount("/coverage/static", StaticFiles(directory="app/static"), name="static")
+# ✅ TEMPLATE FIX
 templates = Jinja2Templates(
     directory=os.path.join(BASE_DIR, "templates")
 )
 
+# ✅ CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # bisa dibatasi nanti
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
